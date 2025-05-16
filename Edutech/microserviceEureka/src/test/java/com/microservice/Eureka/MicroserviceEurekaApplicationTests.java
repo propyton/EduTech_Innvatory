175f3ee60ca08557e759af1a31662556c8fde8c0
Diego Ignacio Figueroa Varas <diegofigueroavaras@gmail.com>
2025-05-16 00:24:03 -0400

    configuracion de ureka mas los microservicios

---
+++ Edutech/microserviceEureka/src/test/java/com/microservice/Eureka/MicroserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
